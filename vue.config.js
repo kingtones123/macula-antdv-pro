ab5b76db09ab82dd4c0750e51e453239e6e168b6
Rain Wang <rainwzp@gmail.com>
2020-09-28 09:37:58 +0800

    设置上下文路径static

--- vue.config.js
+++ vue.config.js
@@ -154,8 +154,6 @@ const vueConfig = {
   /* ADVANCED SETTINGS */
   publicPath: process.env.VUE_APP_CONTEXT_PATH,
 
-  assetsDir: 'static',
-
   // disable source map in production
   productionSourceMap: !isProd,
   // ESLint Check: DISABLE for false
